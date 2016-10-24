c2fb953ef51b758af9c58b96c59b78f711a35101
Aliasgar Murtaza <aliasgarlabs@gmail.com>
2016-10-24 21:05:21 +0800

    Added authentication

--- app/src/main/java/com/aliasgarmurtaza/ntuosschat/MainActivity.java
+++ app/src/main/java/com/aliasgarmurtaza/ntuosschat/MainActivity.java
@@ -2,15 +2,23 @@ package com.aliasgarmurtaza.ntuosschat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -23,6 +31,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     //Declaring FirebaseAuth object
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +54,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //Initializing FirebaseAuth
         mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+
+            }
+        };
 
 
     }
@@ -78,14 +103,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
     private void signInOrSignUp() {
 
         String name, password, confirmPassword, email;
 
         name = etName.getText().toString();
-        email = etName.getText().toString();
-        password = etName.getText().toString();
-        confirmPassword = etName.getText().toString();
+        email = etEmail.getText().toString();
+        password = etPassword.getText().toString();
+        confirmPassword = etConfirmPassword.getText().toString();
 
 
         boolean validated = validate(name, email, password, confirmPassword);
@@ -95,16 +134,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //Proceed with Sign In or Sign UP
             if (signUpFeaturesVisible) {
                 //Sign Up
-                //TODO Add Sign Up functionality
+             createUser(name, email,password);
 
-                //TODO Dummy login - Should be removed after implementation
-                openMessagesActivity();
             } else {
                 //Sign In
-                //TODO Add Sign In functionality
-
-                //TODO Dummy login - Should be removed after implementation
-                openMessagesActivity();
+             signInUser(email,password);
             }
         } else {
             Toast.makeText(getApplicationContext(), "Please check your details", Toast.LENGTH_LONG).show();
@@ -127,5 +161,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (!password.equals(confirmPassword) || password.isEmpty())
             return false;
         return true;
+    }
+
+    private void createUser(final String name, final String email, String password)
+    {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Sign up failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            updateUserInDB(task.getResult().getUser().getUid(), name, email);
+                            openMessagesActivity();
+                        }
+                    }
+                });
+    }
+
+    private void signInUser(String email, String password)
+    {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(getApplicationContext(), "Sign in failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            openMessagesActivity();
+                        }
+
+                    }
+                });
+    }
+
+    private void updateUserInDB(String uid, String name, String email) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users/"+uid);
+        myRef.child("name").setValue(name);
+        myRef.child("email").setValue(email);
     }
 }
